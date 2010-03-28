63e83eec1d8b18ca668f456329b82ddbe58d7da4
Ruth Leibig <releibig@gmail.com>
2010-03-28 19:14:29 +0000

    create event controller. Add prototype some prototype views to test it. Tweak event model to require title to be present
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@7 1f962665-22ff-41fc-9765-37673d54b4f3

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,35 @@
+cclass EventsController < ApplicationController
+  def index
+    @events = Event.find :all
+  end
+  def show
+    @event = Event.find(params[:id])
+  end
+  def create
+    event = Event.new(params[:event])
+    if (event.save)
+       redirect_to :action => 'show', :id => event
+    end 
+  end
+  def new
+    @event = Event.new
+  end
+  
+  def edit
+    @event = Event.find(param[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(params[:event])
+      redirect_to :action => 'show', :id => @event
+    else
+      render :action => edit
+    end
+  end
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to :action => index
+  end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-
+  validates_presence_of :title
   has_many  :event_assignments
   has_many  :people, :through => :event_assignments
   
