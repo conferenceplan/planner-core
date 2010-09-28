6f199d4e92506b4dacb57061d21aba01a6c3650d
Henry Balen <balen@acm.org>
2010-09-28 15:24:44 +0000

    minor cleanup to the session/login/logout handling
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@58 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/routes.rb
+++ config/routes.rb
@@ -67,8 +67,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :rooms
 
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
-  
+  map.root :controller => "user_sessions", :action => "new"
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
+
   map.resource :account, :controller => "users"
   map.resources :users
     
