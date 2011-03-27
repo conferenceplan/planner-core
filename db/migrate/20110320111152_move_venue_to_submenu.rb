a72ed271d63a6c54e40ce137c4cb16caf0f0a3aa
Cathy Mullican <menolly@mib.org>
2011-03-27 02:19:46 +0000

    fix venue meni move
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@334 1f962665-22ff-41fc-9765-37673d54b4f3

--- db/migrate/20110320111152_move_venue_to_submenu.rb
+++ db/migrate/20110320111152_move_venue_to_submenu.rb
@@ -1,9 +1,16 @@
 class MoveVenueToSubmenu < ActiveRecord::Migration
   def self.up
-     execute "update menu_items set menu_id = NULL, menu_item_id = 41 where id = 47;"
+    menu = MenuItem.find_by_name( "Rooms" )
+    item = MenuItem.find_by_name( "Venues" )
+    item.menu_item_id = menu.id
+    item.menu_id = nil
+    item.save
   end
 
   def self.down
-     execute "update menu_items set menu_id = 4, menu_item_id = NULL where id = 47;"
+    menu = Menu.find_by_title( "Main" )
+    item = MenuItem.find_by_name( "Venues" )
+    item.menu_id = menu.id
+    item.save
   end
 end
