9d602524d8a69072a96c2b9577b0c59f7f1d5e5a
Henry Balen <balen@acm.org>
2011-03-27 07:48:43 +0000

    tiedied up the move venue migration
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@335 1f962665-22ff-41fc-9765-37673d54b4f3

--- db/migrate/20110325153114_correctly_move_venue_menu.rb
+++ db/migrate/20110325153114_correctly_move_venue_menu.rb
@@ -1,51 +1,17 @@
 class CorrectlyMoveVenueMenu < ActiveRecord::Migration
   def self.up
-   execute <<-SQL
-      DELETE From menus; 
-    SQL
-    execute <<-SQL
-      DELETE From menu_items;
-    SQL
-    menu = Menu.create(:title => "Main")
-    MenuItem.create( :name => "Home", :path => "/", :menu => menu)
-    MenuItem.create( :name => "Items", :path => "/programme_items", :menu => menu)
-    partMenu = MenuItem.create( :name => "Participants", :path => "/participants", :menu => menu)
-    MenuItem.create( :name => "New", :path => "/participants/new", :menu_item => partMenu)
-    MenuItem.create( :name => "Import", :path => "/pending_import_people/import", :menu_item => partMenu)
-    MenuItem.create( :name => "Set Invite Pending to Invited", :path => "/participants/SetInvitePendingToInvited", :menu_item => partMenu)
-    MenuItem.create( :name => "Invitation Categories", :path => "/invitation_categories", :menu_item => partMenu)
-    MenuItem.create( :name => "Schedule", :path => "/", :menu => menu)
-    comMenu = MenuItem.create( :name => "Communications", :path => "/", :menu => menu)
-    MenuItem.create(:name => "Export email list", :path => "/participants/exportemailxml", :menu_item => comMenu)
-    roomMenu = MenuItem.create(:name => "Rooms", :path => "/rooms", :menu => menu)
-    MenuItem.create(:name => "Venues", :path => "/venue", :menu_item => roomMenu)
-    adminMenu = MenuItem.create(:name => "Admin", :path => "/", :menu => menu)
-    MenuItem.create( :name => "Users", :path => "/usersadmin", :menu_item => adminMenu)
-    MenuItem.create( :name => "Tags", :path => "/", :menu_item => adminMenu)
+    roomMenu = MenuItem.all(:conditions => { :name => "Rooms" } ).first
+    menu = MenuItem.all(:conditions => { :name => "Venues" } ).first
+    menu.menu = nil
+    menu.menu_item = roomMenu
+    menu.save
   end
 
   def self.down
-    execute <<-SQL
-      DELETE From menus; 
-    SQL
-    execute <<-SQL
-      DELETE From menu_items;
-    SQL
-    menu = Menu.create(:title => "Main")
-    MenuItem.create( :name => "Home", :path => "/", :menu => menu)
-    MenuItem.create( :name => "Items", :path => "/programme_items", :menu => menu)
-    partMenu = MenuItem.create( :name => "Participants", :path => "/participants", :menu => menu)
-    MenuItem.create( :name => "New", :path => "/participants/new", :menu_item => partMenu)
-    MenuItem.create( :name => "Import", :path => "/pending_import_people/import", :menu_item => partMenu)
-    MenuItem.create( :name => "Set Invite Pending to Invited", :path => "/participants/SetInvitePendingToInvited", :menu_item => partMenu)
-    MenuItem.create( :name => "Invitation Categories", :path => "/invitation_categories", :menu_item => partMenu)
-    MenuItem.create( :name => "Schedule", :path => "/", :menu => menu)
-    comMenu = MenuItem.create( :name => "Communications", :path => "/", :menu => menu)
-    MenuItem.create(:name => "Export email list", :path => "/participants/exportemailxml", :menu_item => comMenu)
-    MenuItem.create(:name => "Rooms", :path => "/rooms", :menu => menu)
-    MenuItem.create(:name => "Venues", :path => "/venue", :menu => menu)
-    adminMenu = MenuItem.create(:name => "Admin", :path => "/", :menu => menu)
-    MenuItem.create( :name => "Users", :path => "/usersadmin", :menu_item => adminMenu)
-    MenuItem.create( :name => "Tags", :path => "/", :menu_item => adminMenu)
+    rootMenu = Menu.find_by_title("Main")
+    menu = MenuItem.all(:conditions => { :name => "Venues" } ).first
+    menu.menu = rootMenu
+    menu.menu_item = nil
+    menu.save
   end
 end
