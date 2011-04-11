948f4cb4d2d9c03f9ee4388607a9b26d0a8f2477
Henry Balen <balen@acm.org>
2011-04-11 05:32:07 +0000

    removed un-needed migration
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@365 1f962665-22ff-41fc-9765-37673d54b4f3

--- db/migrate/20110410222649_fix_programme_item_room_and_time_relation.rb
+++
@@ -1,9 +0,0 @@
-class FixProgrammeItemRoomAndTimeRelation < ActiveRecord::Migration
-  def self.up
-      remove_column :rooms, :room_id
-      remove_column :rooms, :time_slot_id
-  end
-
-  def self.down
-  end
-end
