0394d35311c821c558fb4e6f6a0d6637efa29eed
Henry Balen <balen@acm.org>
2011-04-16 22:48:21 +0000

    Get rid of timeslots with 0 time before doing the defrag
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@395 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/program_planner_controller.rb
+++ app/controllers/program_planner_controller.rb
@@ -100,10 +100,10 @@ class ProgramPlannerController < PlannerController
     room = candidate.room
     candidate.programme_item = nil
     candidate.save
-
+    
     # 2. Take the time slot and merge back into free time
     defragmentFreeTimes(room, day)
-
+    
     render :layout => 'content'
   end
   
@@ -115,8 +115,8 @@ class ProgramPlannerController < PlannerController
     end
     query += CONFLICT_QUERY_PT2
     @conflicts = ActiveRecord::Base.connection.select_rows(query)
-#    Room.find_by_sql(CONFLICT_QUERY)
-
+    #    Room.find_by_sql(CONFLICT_QUERY)
+    
     respond_to do |format|
       format.html { 
         if @day
@@ -125,13 +125,27 @@ class ProgramPlannerController < PlannerController
       }
     end
   end
-
+  
   protected
   
   # Defragment the free times for this room on the given day !!
   # 1. get the free times for the room - 
   # i.e. the room item assigments that do not have a programme item associated with them, order by start time
   def defragmentFreeTimes(room, day)
+    RoomItemAssignment.transaction do # ensure that all the changes are done within the one transaction
+      emptyTimes = RoomItemAssignment.all(:conditions => ["room_id = ? AND day = ? AND programme_item_id is null AND time_slots.start = time_slots.end", room.id, day],
+                          :joins => "LEFT JOIN time_slots ON time_slots.id = time_slot_id ",
+                          :order => "time_slots.start asc" )
+      if emptyTimes.size > 0
+        for idx in 0 ... emptyTimes.size
+          ts = TimeSlot.find(emptyTimes[idx].time_slot.id)
+          f = RoomItemAssignment.find(emptyTimes[idx].id)
+          ts.destroy
+          f.destroy
+        end
+      end
+    end
+    
     RoomItemAssignment.transaction do # ensure that all the changes are done within the one transaction
       fts = RoomItemAssignment.all(:conditions => ["room_id = ? AND day = ? AND programme_item_id is null", room.id, day],
                           :joins => "LEFT JOIN time_slots ON time_slots.id = time_slot_id ",
@@ -154,41 +168,41 @@ class ProgramPlannerController < PlannerController
     end
   end
   
-  CONFLICT_QUERY_PT1 = <<"EOS"
-select 
-R.id id, R.name name,
-P.id person_id, P.first_name person_first_name, P.last_name, 
-S.id item_id, S.title item_name, Conflicts.startA item_start,
-RB.id conflict_room_id, RB.name conflict_room_name,
-SB.id conflict_item_id, SB.title conflict_item_title,
-Conflicts.startB conflict_start
-from people P, rooms R, programme_items S,
-rooms RB, programme_items SB, 
-(select progA.person_id pidA, progB.person_id pidB, 
-roomA.room_id roomA, progA.programme_item_id progA, 
-progA.role_id roleA, progB.role_id roleB, tsA.start startA, tsA.end endA,
-roomB.room_id roomB, progB.programme_item_id progB, tsB.start startB, tsA.end endB
-from programme_item_assignments progA, room_item_assignments roomA, time_slots tsA,
-programme_item_assignments progB, room_item_assignments roomB, time_slots tsB
-where
-roomA.programme_item_id = progA.programme_item_id AND roomA.time_slot_id = tsA.id AND
-roomB.programme_item_id = progB.programme_item_id AND roomB.time_slot_id = tsB.id AND
-(tsA.start < tsB.start OR (tsA.start = tsB.start AND progA.programme_item_id < progB.programme_item_id))
-AND tsA.end > tsB.start
-AND progA.programme_item_id <> progB.programme_item_id
-AND progA.person_id = progB.person_id
-AND progA.role_id = progB.role_id
+CONFLICT_QUERY_PT1 = <<"EOS"
+  select 
+  R.id id, R.name name,
+  P.id person_id, P.first_name person_first_name, P.last_name, 
+  S.id item_id, S.title item_name, Conflicts.startA item_start,
+  RB.id conflict_room_id, RB.name conflict_room_name,
+  SB.id conflict_item_id, SB.title conflict_item_title,
+  Conflicts.startB conflict_start
+  from people P, rooms R, programme_items S,
+  rooms RB, programme_items SB, 
+   (select progA.person_id pidA, progB.person_id pidB, 
+   roomA.room_id roomA, progA.programme_item_id progA, 
+   progA.role_id roleA, progB.role_id roleB, tsA.start startA, tsA.end endA,
+   roomB.room_id roomB, progB.programme_item_id progB, tsB.start startB, tsA.end endB
+   from programme_item_assignments progA, room_item_assignments roomA, time_slots tsA,
+   programme_item_assignments progB, room_item_assignments roomB, time_slots tsB
+   where
+   roomA.programme_item_id = progA.programme_item_id AND roomA.time_slot_id = tsA.id AND
+   roomB.programme_item_id = progB.programme_item_id AND roomB.time_slot_id = tsB.id AND
+   (tsA.start < tsB.start OR (tsA.start = tsB.start AND progA.programme_item_id < progB.programme_item_id))
+  AND tsA.end > tsB.start
+  AND progA.programme_item_id <> progB.programme_item_id
+  AND progA.person_id = progB.person_id
+  AND progA.role_id = progB.role_id
 EOS
-
-  CONFLICT_QUERY_PT2 = <<"EOS"
-) as Conflicts
-where
-R.id = Conflicts.roomA AND
-P.id = Conflicts.pidA AND
-S.id = Conflicts.progA AND
-RB.id = Conflicts.roomB AND
-SB.id = Conflicts.progB
-order by P.id
+  
+CONFLICT_QUERY_PT2 = <<"EOS"
+  ) as Conflicts
+  where
+  R.id = Conflicts.roomA AND
+  P.id = Conflicts.pidA AND
+  S.id = Conflicts.progA AND
+  RB.id = Conflicts.roomB AND
+  SB.id = Conflicts.progB
+  order by P.id
 EOS
-
+  
 end
