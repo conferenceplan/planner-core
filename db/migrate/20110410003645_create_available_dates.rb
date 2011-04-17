ce1aa63f6e9f0a0ddcaa5006ca5f209108e9bb80
Ruth Leibig <releibig@gmail.com>
2011-04-17 21:32:52 +0000

    Fix where end is in migration
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@399 1f962665-22ff-41fc-9765-37673d54b4f3

--- db/migrate/20110410003645_create_available_dates.rb
+++ db/migrate/20110410003645_create_available_dates.rb
@@ -2,15 +2,16 @@ class CreateAvailableDates < ActiveRecord::Migration
   def self.up
       create_table :available_dates do |t|
       
-      t.datetime :start_time
-      t.datetime :end_time
-      t.references :person
-      t.timestamps
-      t.column :lock_version, :integer, { :default => 0 }
+       t.datetime :start_time
+       t.datetime :end_time
+       t.references :person
+       t.timestamps
+       t.column :lock_version, :integer, { :default => 0 }
+   end
   end
 
   def self.down
     drop_table  :available_dates
   end
-  end
+
 end
