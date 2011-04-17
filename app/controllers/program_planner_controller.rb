fe694bbb50d5d18acef7183bb33093acd472d1a9
Henry Balen <balen@acm.org>
2011-04-17 13:38:19 +0000

    fix for order of rooms in schedule
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@397 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/program_planner_controller.rb
+++ app/controllers/program_planner_controller.rb
@@ -13,8 +13,8 @@ class ProgramPlannerController < PlannerController
     
     # We need to know which rooms we are going for...
     args = { :conditions => clause }
-    args.merge!(:joins => 'LEFT JOIN time_slots ON time_slots.id = time_slot_id LEFT JOIN rooms ON rooms.id = room_id')
-    args.merge!(:order => 'rooms.id, time_slots.start asc')
+    args.merge!(:joins => 'LEFT JOIN time_slots ON time_slots.id = time_slot_id LEFT JOIN rooms ON rooms.id = room_id LEFT JOIN venues ON venues.id = rooms.venue_id')
+    args.merge!(:order => 'rooms.name asc, time_slots.start asc')
     args.merge!(:include => [:time_slot, :programme_item, :room]) # eager loads the associated objects to reduce number of queries
     
     @roomAssignments = RoomItemAssignment.find :all, args
