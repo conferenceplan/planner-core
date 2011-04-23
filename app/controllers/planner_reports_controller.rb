bd47336ee0df4b3576dcedf92fb7a44e43fb117b
Cathy Mullican <menolly@mib.org>
2011-04-23 03:59:49 +0000

    flag reserved participants, add details to CSV report
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@402 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/planner_reports_controller.rb
+++ app/controllers/planner_reports_controller.rb
@@ -25,22 +25,41 @@ class PlannerReportsController < PlannerController
        mod_date = params[:mod_date]
     end
 
-    panels = ProgrammeItem.all(:include => :people, :conditions => ["programme_items.updated_at >= ? or programme_item_assignments.updated_at >= ? or (programme_items.updated_at is NULL and programme_items.created_at >= ?) or (programme_item_assignments.updated_at is NULL and programme_item_assignments.created_at >= ?)", mod_date, mod_date, mod_date, mod_date], :order => "programme_items.id, people.last_name") 
+    panels = ProgrammeItem.all(:include => [:people, :time_slot, :room, :format,], :conditions => ["programme_items.updated_at >= ? or programme_item_assignments.updated_at >= ? or (programme_items.updated_at is NULL and programme_items.created_at >= ?) or (programme_item_assignments.updated_at is NULL and programme_item_assignments.created_at >= ?)", mod_date, mod_date, mod_date, mod_date], :order => "programme_items.id, people.last_name") 
 
     csv_data = FasterCSV.generate do |csv|
        csv << [
           "Panel",
+          "Format",
+          "Room",
+          "Venue",
+          "Time Slot",
           "Participants",
+          "Reserved",
        ]
        panels.each do |panel|
          names = Array.new
+         rsvd = Array.new
          panel.people.each do |p|
-            names.push p.GetFullName.strip
+            a = ProgrammeItemAssignment.first(:conditions => {:person_id => p.id, :programme_item_id => panel.id})
+            if a.role_id == 16
+               names.push "#{p.GetFullName.strip} (M)"
+            elsif a.role_id == 18
+               rsvd.push p.GetFullName.strip
+            else
+               names.push p.GetFullName.strip
+            end
          end
          part_list = names.join(', ')
+         rsvd_list = rsvd.join(', ')
          csv << [
             panel.title,
-            part_list
+            panel.format.name,
+            (panel.room.nil?) ? '' : panel.room.name,
+            (panel.room.nil?) ? '' : Venue.find(panel.room.id).name,
+            (panel.time_slot.nil?) ? '' : "#{panel.time_slot.start.strftime('%a %H:%M')} - #{panel.time_slot.end.strftime('%H:%M')}",
+            part_list,
+            rsvd_list,
          ]
        end
      end
@@ -61,10 +80,17 @@ class PlannerReportsController < PlannerController
     
     @names.each do |name|
        panels = Array.new
-       name.programmeItems.find_each(:include => [:time_slot, :room, :format]) do |p|
+       name.programmeItems.find_each(:include => [:time_slot, :room, :format, ]) do |p|
+          a = ProgrammeItemAssignment.first(:conditions => {:person_id => name.id, :programme_item_id => p.id})
           panelstr = "#{p.title} (#{p.format.name})"
+          if a.role_id == 16
+             panelster << " (M)"
+          end
           panelstr << ", #{p.time_slot.start.strftime('%a %H:%M')} - #{p.time_slot.end.strftime('%H:%M')}" unless p.time_slot.nil?
           panelstr << ", #{p.room.name} (#{Venue.find(p.room.venue_id).name})" unless p.room.nil?
+          if a.role_id == 18
+             panelstr = "<i>#{panelstr}</i>"
+          end
           if p.time_slot.nil?
              zeroTime = Time.at(0)
              panels.push [zeroTime, "<li>#{panelstr}</li>"]
