75ace6cc1f05c99ea3576bae811296bdd5613d0c
Henry Balen <balen@acm.org>
2011-04-24 15:01:11 +0000

    Tidy up free time logic
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@405 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,16 +1,2 @@
 module RoomsHelper
-
-  def getFreeTimes(room, day)
-    # TODO - change so that it does a query
-    free_times = room.free_times
-    
-    if free_times = nil || free_times.size == 0
-      free_times = room.free_times.new(
-#        :day => day,
-         :start_time => SITE_CONFIG[:conference][:start_date],
-         :end_time => SITE_CONFIG[:conference][:start_date]
-        )
-    end
-  end
-
 end
