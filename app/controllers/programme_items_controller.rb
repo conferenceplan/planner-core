70e675f55d3fcfc95d41083c70d905ba668f518d
Henry Balen <balen@acm.org>
2011-05-02 12:08:26 +0000

    fixed too many joins of same table for item query when using tags
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@437 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/programme_items_controller.rb
+++ app/controllers/programme_items_controller.rb
@@ -151,11 +151,12 @@ class ProgrammeItemsController < PlannerController
 
     args = { :conditions => clause }
     
+#    LEFT JOIN time_slots on time_slots.id = room_item_assignments.time_slot_id
     args.merge!( :joins => 'LEFT JOIN room_item_assignments ON room_item_assignments.programme_item_id = programme_items.id LEFT JOIN time_slots on time_slots.id = room_item_assignments.time_slot_id' )
 
     # First we need to know how many records there are in the database
     # Then we get the actual data we want from the DB
-    args.merge!(:include => :format)
+#    args.merge!(:include => :format)
     
     tagquery = ""
     if context
@@ -180,7 +181,7 @@ class ProgrammeItemsController < PlannerController
     @nbr_pages += 1 if @count % rows.to_i > 0
     
     offset = (@page.to_i - 1) * rows.to_i
-    args.merge!(:offset => offset, :limit => rows, :order => "time_slots.start asc, " + idx + " " + order, :include => [:room, :time_slot])
+    args.merge!(:offset => offset, :limit => rows, :order => "time_slots.start asc, " + idx + " " + order)
     if tagquery.empty?
       @programmeItems = ProgrammeItem.find :all, args
     else
