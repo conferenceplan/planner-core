04c6ead01d7b51552f4cfa2e91371ccdc8d6d0d3
Henry Balen <balen@acm.org>
2011-05-12 02:31:34 +0000

    fix for interaction of time format with config
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@448 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/environment.rb
+++ config/environment.rb
@@ -42,8 +42,6 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'Pacific Time (US & Canada)'
  
-  Time::DATE_FORMATS[:weekdatetime] = "%A, %d %b %H:%M"
-  
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
@@ -53,3 +51,5 @@ Rails::Initializer.run do |config|
 #  end
   
 end
+
+Time::DATE_FORMATS[:weekdatetime] = "%A, %d %b %H:%M"
