5e01b7f4f8d08b4c8d491ebc45a7ea440d1e33be
Jim Rootham <jrootham@gmail.com>
2011-06-12 21:08:06 +0000

    Implement room setups
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@503 1f962665-22ff-41fc-9765-37673d54b4f3

--- public/javascripts/rooms/rooms.js
+++ public/javascripts/rooms/rooms.js
@@ -2,207 +2,95 @@
  *
  */
 jQuery(document).ready(function(){
-    // The grid containing the list of rooms
-    jQuery("#rooms").jqGrid({
-        url: '/rooms/list',
-        datatype: 'xml',
-        colNames: ['Room Name', 'Venue', 'Capacity','Purpose','Comment','lock_version'],
-        colModel: [
-		{
-            name: 'room[name]',
-            index: 'name',
-            width: 125,
-            searchoptions: {
-                sopt: ['eq','ne','bw','bn','cn','nc']
-            },
-            editable: true,
-            editoptions: {
-                size: 20
-            },
-            formoptions: {
-                rowpos: 1,
-                label: "Name:",
-                elmprefix: "(*)"
-            },
-            editrules: {
-                required: true
-            },
-        },{
-            name: 'room[venue_id]',
-            index: 'venue_id',
-            width: 125,
-            editable: true,
-	    search: true,
-            stype: 'select',
-            searchoptions: {
-                dataUrl:'/venue/list',
-                sopt: ['eq','ne']
-            },
-            sortable: true,
-            edittype: "select",
-            editoptions: {
-                dataUrl:'/venue/list'
-            },
-            formoptions: {
-                rowpos: 2,
-                label: "Venue:",
-                elmprefix: "(*)"
-            },
-            editrules: {
-                required: true
-            },
-        },{
-            name: 'room[capacity]',
-            index: 'capacity',
-            width: 75,
-            editable: true,
-	    search: true,
-            searchoptions: {
-                sopt: ['eq','ne','ge','le']
-            },
-            sortable: true,
-            editoptions: {
-                size: 20
-            },
-            formoptions: {
-                rowpos: 3,
-                label: "Capacity:",
-            },
-            editrules: {
-                required: false
-            },
-        },{
-            name: 'room[purpose]',
-            index: 'purpose',
-            width: 170,
-            editable: true,
-	    search: true,
-            searchoptions: {
-                sopt: ['eq','ne','bw','bn','cn','nc']
-            },
-            editoptions: {
-                size: 20
-            },
-            formoptions: {
-                rowpos: 4,
-                label: "Purpose:",
-            },
-            editrules: {
-                required: false
-            },
-        },{
-            name: 'room[comment]',
-            index: 'comment',
-            width: 170,
-            editable: true,
-            hidden: true,
-            viewable: true,
-	    search: true,
-            searchoptions: {
-                sopt: ['eq','ne','bw','bn','cn','nc']
-            },
-            edittype: "textarea",
-            formoptions: {
-                rowpos: 5,
-                label: "Comment:",
-            },
-            editrules: {
-                required: false,
-                edithidden: true
-            },
-        },{
-            name: 'room[lock_version]',
-            index: 'lock_version',
-            hidden: true,
-	    search: false,
-        }
-		],
-        pager: jQuery('#pager'),
-        rowNum: 10,
-        autowidth: false,
-        rowList: [10, 20, 30],
-        pager: jQuery('#pager'),
-        sortname: 'name',
-        sortorder: "asc",
-        viewrecords: true,
-        imgpath: 'stylesheets/start/images',
-        caption: 'Rooms',
-        editurl: '/rooms',
-        onSelectRow: function(ids){
-            $('#room_id').text(ids);
-            return false;
-        }
-    });
-    
-    // Set up the pager menu for add, delete, and search
-    jQuery("#rooms").navGrid('#pager', {
-       view: true,
-    }, //options
-    { // edit options
-        height: 250,
-        reloadAfterSubmit: false,
-        closeAfterEdit: true,
-        jqModal: true,
-        closeOnEscape: true,
-        bottominfo: "Fields marked with (*) are required",
-        afterSubmit: processResponse,
-        beforeSubmit: function(postdata, formid){
-            this.ajaxEditOptions = {
-                url: '/rooms/' + postdata.rooms_id, // TODO
-                type: 'put'
-            };
-            return [true, "ok"];
-        }
-    }, // edit options
-    { // add options
-        height: 250,
-        reloadAfterSubmit: true,
-        jqModal: true,
-        closeOnEscape: true,
-        bottominfo: "Fields marked with (*) are required",
-        afterSubmit: processResponse,
-        closeAfterAdd: true
-    }, // add options
-    { // del options
-        reloadAfterSubmit: true,
-        jqModal: true,
-        closeOnEscape: true,
-        beforeSubmit: function(postdata){
-            this.ajaxDelOptions = {
-                url: '/rooms/' + postdata,
-                type: 'delete'
-            };
-            return [true, "ok"];
-        },
-    }, 
-    { // search options
-	jqModal:true, closeOnEscape:true,
-	multipleSearch:true,
-	//sopt:['eq','ne','le','ge'],
-	//odata: ['begins with', 'does not begin with'],
-	groupOps: [ { op: "AND", text: "all" }, { op: "OR", text: "any" } ]
-    }, // search options
-    { //view options
-        caption: "Room",
-	bClose: "Close",
-        beforeShowForm: function(formid){
-           $('.ui-jqdialog-content .form-view-data').css('white-space', 'normal');
-           $('.ui-jqdialog-content .form-view-data').css('height', 'auto');
-           $('.ui-jqdialog-content .form-view-data').css('vertical-align', 'text-top');
-           $('.ui-jqdialog-content .form-view-data').css('padding-top', '2px');
-        },
-    } //view options
-	);
-});
+	
+	var setupGrid = function
+	(
+		room_id				// Id of the room this set of room setups applies to
+	){
+        var index = 0;
+		var columns = [];
+		var pickroom = 'rooms/picklist?room_id=' + room_id;
+		columns[index++] = $.planIt.select(['room_setups', 'room_id'], 255, 1, "Room:", true, pickroom);
+		columns[index++] = $.planIt.select(['room_setups', 'setup_type_id'], 225, 2, "Setup type:", true, 'setup_types/picklist');
+		columns[index++] = $.planIt.textSearch(['room_setups', 'capacity'], 225, 3, "Capacity:", true);
+		columns[index++] = $.planIt.hidden(['room_setups', 'lock_version']);
+		
+	    var colNames = ['Room', 'Type', 'Capacity', 'lock_version'];
+	 	
+	    var params = $.planIt.gridParams('room_setups', 'room_setups_id', colNames, columns, 'Room Setups');
+	    params.url = 'room_setups/list/?room_id=' + room_id;
+		params.pager = "#setups_pager";
 
-function processResponse(response, postdata){
-    // examine return for problem - look for errorExplanation in the returned HTML
-    var text = $(response.responseText).find(".errorExplanation");
-    if (text.size() > 0) {
-        text.css('font-size', '6pt');
-        text = $("<div></div>").append(text);
-        return [false, text.html()];
+		jQuery("#room_setups").jqGrid(params);
+
+        // Set up the pager menu for edit, add, and delete
+
+		var editFetch = function(postData)
+		{
+			return postData.room_setups_id;
+		};
+
+    	var edit = $.planIt.editOptions('room_setups', editFetch);
+    	var add = $.planIt.addOptions('room_setups');
+    	var del = $.planIt.deleteOptions('room_setups');
+    	
+        jQuery("#room_setups").navGrid('#setups_pager', {search:false, view:false}, edit, add, del);
+	};
+	
+	var clearGrid = function()
+	{
+		jQuery('#setups').html('<table id="room_setups"></table><div id="setups_pager"></div>');
+	};
+	
+    // The grid containing the list of rooms
+	
+	var index = 0;
+	var columns = [];
+	columns[index++] = $.planIt.textSearch(['room', 'name'], 125, 1, "Name:", true);
+	columns[index++] = $.planIt.select(['room', 'venue_id'], 125, 2, "Venue:", true, 'venue/list');
+	var setup = $.planIt.select(['room', 'room_setup_id'], 125, 3, "Room Setup:", false, 'room_setups/picklist');
+	setup.editable = false;
+	columns[index++] = setup;
+	var capacity = $.planIt.textSearch(['room', 'capacity'], 125, 4, "Capacity:", false);
+	capacity.editable = false;
+	columns[index++] = capacity;
+	columns[index++] = $.planIt.textSearch(['room', 'purpose'], 125, 5, "Purpose:", false);
+	columns[index++] = $.planIt.textarea(['room', 'comment'], 125, 6, "Comment:", false);
+	columns[index++] = $.planIt.hidden(['room', 'id']);
+	columns[index++] = $.planIt.hidden(['room', 'lock_version']);
+	
+    var colNames = ['Room Name', 'Venue', 'Setup', 'Capacity', 'Purpose', 'Comment', 'room_id', 'lock_version'];
+ 	
+    var params = $.planIt.gridParams('rooms', 'room_id', colNames, columns, 'Rooms');
+
+    params.onSelectRow = function (id) {
+    	var room_id;
+        if (id == null) {
+        	clearGrid();
+        }
+        else {
+        	var row_data = jQuery("#rooms").getRowData(id);
+        	room_id = row_data["room[id]"];
+        	clearGrid();
+        	setupGrid(room_id);
+        }
     }
- jQuery("#rooms").trigger("reloadGrid");
-    return [true, "Success"];
-}
+            		
+    jQuery("#rooms").jqGrid(params);
+    
+	var editFetch = function(postData)
+	{
+		return postData.rooms_id;
+	};
+
+    // Set up the pager menu for edit, add, and delete
+
+	var edit = $.planIt.editOptions('rooms', editFetch);
+	var add = $.planIt.addOptions('rooms');
+	var del = $.planIt.deleteOptions('rooms');
+	
+    jQuery("#rooms").navGrid('#pager', {search:false, view:false}, edit, add, del);
+    
+//    setupGrid();
+    });
+
