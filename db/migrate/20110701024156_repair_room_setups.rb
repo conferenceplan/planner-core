f865255fb36c8498d49035492cf636034233b2a6
Jim Rootham <jrootham@gmail.com>
2011-07-01 17:33:45 +0000

    Fix room setups
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@539 1f962665-22ff-41fc-9765-37673d54b4f3

---
+++ db/migrate/20110701024156_repair_room_setups.rb
@@ -0,0 +1,18 @@
+class RepairRoomSetups < ActiveRecord::Migration
+  def self.up
+    add_column :rooms, :setup_id, :integer    #  Default
+    Room.reset_column_information
+
+    Room.all.each do |room|
+      roomSetup = RoomSetup.find_by_room_id room.id
+      room.setup_id = roomSetup.id
+      room.save
+    end
+    
+    remove_column :rooms, :setup_type_id
+
+  end
+
+  def self.down
+  end
+end
