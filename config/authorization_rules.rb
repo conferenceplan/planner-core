254fbcde82f4282794a45785828f6a9b7a8ff778
Henry Balen <balen@acm.org>
2011-07-25 11:28:56 +0000

    Added review to the menu
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/PlannerPrototype@594 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -29,6 +29,7 @@ authorization do
     has_permission_on :available_dates, :to => [:read]
     has_permission_on :equipment_types, :to => :read
     has_permission_on :equipment_needs, :to => :read
+    has_permission_on :publisher, :to => :review
   end
 
   role :TagAdmin do
@@ -113,6 +114,7 @@ authorization do
 end
 
 privileges do
+    privilege :review, :includes => [:review]
     privilege :report, :includes => [:library_talks,:missing_bio,:moderators,:art_night,:music_night,:program_types,:free_text,:tags_by_context,:available_during,:panels_with_panelists,:panelists_with_panels,:admin_tags_by_context,:panels_date_form,:panelists_with_metadata,:interviewable]
     privilege :read, :includes => [:report, :index, :show, :list, :listwithblank, :comphrensive,:acceptancestatuslist,:acceptancestatuslistwithblank,:ReportInviteStatus,
         :doReportInviteStatus,:invitestatuslist,:invitestatuslistwithblank,:getConflicts,:getRoomControl]
