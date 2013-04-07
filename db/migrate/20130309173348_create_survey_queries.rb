7910ca324379212656ba82e2fe8cd79d3d36770c
Henry Balen <balen@acm.org>
2013-04-07 15:27:25 +0000

    survey report engine
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/Planner@810 1f962665-22ff-41fc-9765-37673d54b4f3

--- db/migrate/20130309173348_create_survey_queries.rb
+++ db/migrate/20130309173348_create_survey_queries.rb
@@ -4,13 +4,17 @@ class CreateSurveyQueries < ActiveRecord::Migration
       
       # Name
       t.string :name
+      # Operation
+      t.boolean :operation # indicate whether to AND or OR this with the rest of the queries
+      
       # Survey id
       t.integer :survey_id
+      
       # Shared
       t.boolean :shared
       # Owner 
 		  t.integer :user_id
-		
+
       t.timestamps
       t.column :lock_version, :integer, { :default => 0 }
     end
--- db/migrate/20130309174000_create_survey_query_predicates.rb
+++ db/migrate/20130309174000_create_survey_query_predicates.rb
@@ -2,18 +2,15 @@ class CreateSurveyQueryPredicates < ActiveRecord::Migration
   def self.up
     create_table :survey_query_predicates do |t|
       
-      # The query that this question is part of
-      t.integer :survey_query_id
-      t.boolean :ored # indicate whether to AND or OR this with the rest of the queries
-      
-      # Survey group id
-      t.integer :survey_group_id
       # Survey question id
       t.integer :survey_question_id
       # Operator (contains, does not contain, = , !=)
-      t.string :operator
+      t.string :operation
       # Value
       t.string :value
+      
+      # Query that this predicate belongs to 
+      t.integer :survey_query_id
 
       t.timestamps
       t.column :lock_version, :integer, { :default => 0 }
@@ -21,6 +18,6 @@ class CreateSurveyQueryPredicates < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table :survey_query_conditions
+    drop_table :survey_query_predicates
   end
 end
