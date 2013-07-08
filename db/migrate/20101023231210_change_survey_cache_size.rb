a9382a25a3b86a056f180947a8bbd71fe4f34a97
Henry Balen <balen@acm.org>
2013-07-08 18:27:15 +0000

    remove of remaining smerf code
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@906 1f962665-22ff-41fc-9765-37673d54b4f3

--- db/migrate/20101023231210_change_survey_cache_size.rb
+++
@@ -1,8 +0,0 @@
-class ChangeSurveyCacheSize < ActiveRecord::Migration
-  def self.up
-    change_column :smerf_forms, :cache, :text, :limit => 100000
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20110209021239_add_has_survey.rb
+++ db/migrate/20110209021239_add_has_survey.rb
@@ -1,9 +1,6 @@
 class AddHasSurvey < ActiveRecord::Migration
   def self.up
     add_column :survey_respondents, :submitted_survey, :boolean, :default => false
-
-    # ensure that it is true for existing respondents that have filled in the survey
-    execute "update survey_respondents set submitted_survey = true where id in (SELECT form.surveyrespondent_id FROM smerf_forms_surveyrespondents form)"
   end
 
   def self.down
