ab91488a8153b793f8136f5d4c114b96c2678936
Henry Balen <balen@acm.org>
2013-07-26 21:38:50 +0000

    asset pipeline and JS cleanup
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@935 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+//= require jquery-1.9.1.min
+//= require jquery-ui-1.9.2.custom.min
 //= require jquery_ujs
+//= require jquery-migrate-1.1.1
+//= require jquery.timers-1.2
+//= require jquery.freeow
+//= require jquery.form
+//= require jquery.timeentry.min
+//= require grid.locale-en
+//= require jquery.jqGrid.min
+
+//= require bootstrap/js/bootstrap
+
+//= require backbone/underscore-min
+//= require backbone/backbone
+//= require backbone/extensions/backbone-relational
+//= require backbone/extensions/backbone.paginator.js
+//= require backgrid/backgrid
+//= require moment/moment
+//= require backgrid/extensions/moment-cell/backgrid-moment-cell
+//= require backgrid/extensions/text-cell/backgrid-text-cell
+
+//= require planIt
 
 $(document).ready(function(){
 		$('li.menutop').hover(
