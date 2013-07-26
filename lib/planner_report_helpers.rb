56f800de4ecb25ed28b0eb4b0f6d07b6a94d6ec2
Henry Balen <balen@acm.org>
2013-07-26 16:03:33 +0000

    Port for R3.2.x - Phase 1 - csv output use ruby 1.9 encode
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@933 1f962665-22ff-41fc-9765-37673d54b4f3

--- lib/planner_report_helpers.rb
+++ lib/planner_report_helpers.rb
@@ -1,34 +1,28 @@
 module PlannerReportHelpers
    require 'csv'
-   require 'iconv'
  
    def csv_out(data, filename)
-      csv_data = CSV.generate do |csv|
+      csv_data = CSV.generate encoding: 'ISO-8859-15' do |csv|
          data.each do |r|
             csv << r
          end
       end
-      c = Iconv.new('ISO-8859-15//IGNORE','UTF-8')
-      send_data c.iconv(csv_data),
-      # send_data csv_data,
-         :type => 'text/csv; charset=iso-8859-1; header=present',
+      send_data csv_data.encode('ISO-8859-15'),
+         :type => 'text/csv; charset=iso-8859-15; header=present',
          :disposition => "attachment; filename=#{filename}"
       flash[:notice] = "Export complete!"
-      
    end
 
    def csv_out_utf16(data, filename)
-      csv_data = CSV.generate do |csv|
+      csv_data = CSV.generate :encoding => 'UTF-16' do |csv|
          data.each do |r|
             csv << r
          end
       end
-      c = Iconv.new('UTF-16//IGNORE','UTF-8')
-      send_data c.iconv(csv_data),
+      send_data csv_data.encode('UTF-16'),
          :type => 'text/csv; charset=utf-16; header=present',
          :disposition => "attachment; filename=#{filename}"
       flash[:notice] = "Export complete!"
-      
    end
 
    def csv_out_noconv(data, filename)
@@ -41,7 +35,6 @@ module PlannerReportHelpers
          :type => 'text/csv; charset=utf-8; header=present',
          :disposition => "attachment; filename=#{filename}"
       flash[:notice] = "Export complete!"
-      
    end
 
    def generate_schedule_csv
