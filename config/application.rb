57bbeb7d3a0e09b2cdd950262ebc463e0d1849ac
Henry Balen <balen@acm.org>
2013-09-27 13:33:12 +0000

    to compile assets
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1053 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,23 @@ module PlannerRc1
 config.middleware.use Rack::JSONP
 
 config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      puts "including asset: " + full_path
+      true
+    else
+      puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
+
     #
     #
     #
