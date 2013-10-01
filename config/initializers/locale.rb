4c4a7909f42d1e6d429b951a437f3b7b527a06a4
Henry Balen <balen@acm.org>
2013-10-01 21:35:08 +0000

    more for language support
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1060 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -9,4 +9,4 @@ I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
 I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
 I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
   
-I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n.backend)
+I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
