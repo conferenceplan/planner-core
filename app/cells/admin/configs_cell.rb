1311b1b1e3c831241dec8f8ef63b3fd9b441c327
Henry Balen <balen@acm.org>
2014-01-31 18:49:49 +0000

    administration functionality
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1368 1f962665-22ff-41fc-9765-37673d54b4f3

---
+++ app/cells/admin/configs_cell.rb
@@ -0,0 +1,15 @@
+class Admin::ConfigsCell < Cell::Rails
+
+  def display
+    render
+  end
+
+  def javascript
+    render
+  end
+
+  def templates
+    render
+  end
+
+end
---
+++ app/cells/admin/manage_cell.rb
@@ -0,0 +1,15 @@
+class Admin::ManageCell < Cell::Rails
+
+  def display
+    render
+  end
+
+  def javascript
+    render
+  end
+
+  def templates
+    render
+  end
+
+end
