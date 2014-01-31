957b22a59a16c3f64a2ef0e6060f7c986ca6e16c
Henry Balen <balen@acm.org>
2014-01-31 16:44:30 +0000

    adding cloudinary support
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1365 1f962665-22ff-41fc-9765-37673d54b4f3

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,12 @@
+#
+# Get these values from the database (assuming that there are values in the db)
+#
+cfg = CloudinaryConfig.find :first # for now we only have one convention... change when we have many (TODO)
+Cloudinary.config do |config|
+  config.cloud_name           = cfg ? cfg.cloud_name : ''
+  config.api_key              = cfg ? cfg.api_key : ''
+  config.api_secret           = cfg ? cfg.api_secret : ''
+  config.enhance_image_tag    = cfg ? cfg.enhance_image_tag : false
+  config.static_image_support = cfg ? cfg.static_image_support : false
+#  config.cdn_subdomain = true
+end
