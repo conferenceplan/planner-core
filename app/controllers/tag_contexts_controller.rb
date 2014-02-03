a05dc24f9d43a1c9f5bc8fbcf67e6d314449a936
Henry Balen <balen@acm.org>
2014-02-03 02:04:24 +0000

    administration functionality
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1385 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/tag_contexts_controller.rb
+++ app/controllers/tag_contexts_controller.rb
@@ -1,86 +1,43 @@
 class TagContextsController < PlannerController
-  # GET /tag_contexts
-  # GET /tag_contexts.xml
+  
   def index
-    @tag_contexts = TagContext.all
+    tag_contexts = TagContext.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @tag_contexts }
-    end
+    render json: tag_contexts.to_json, :content_type => 'application/json'
   end
 
-  # GET /tag_contexts/1
-  # GET /tag_contexts/1.xml
   def show
-    @tag_context = TagContext.find(params[:id])
+    tag_context = TagContext.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @tag_context }
-    end
+    render json: tag_context.to_json, :content_type => 'application/json'
   end
 
-  # GET /tag_contexts/new
-  # GET /tag_contexts/new.xml
-  def new
-    @tag_context = TagContext.new
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @tag_context }
-    end
-  end
-
-  # GET /tag_contexts/1/edit
-  def edit
-    @tag_context = TagContext.find(params[:id])
-  end
-
-  # POST /tag_contexts
-  # POST /tag_contexts.xml
   def create
     # Make sure that the context does not contain a space
     str = params[:tag_context]
     str['name'] = str['name'].gsub(/ /,'_')
-    @tag_context = TagContext.new(str)
+    
+    tag_context = TagContext.new(str)
+    tag_context.save!
 
-    respond_to do |format|
-      if @tag_context.save
-        format.html { redirect_to(@tag_context, :notice => 'TagContext was successfully created.') }
-        format.xml  { render :xml => @tag_context, :status => :created, :location => @tag_context }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @tag_context.errors, :status => :unprocessable_entity }
-      end
-    end
+    render json: tag_context.to_json, :content_type => 'application/json'
   end
 
-  # PUT /tag_contexts/1
-  # PUT /tag_contexts/1.xml
   def update
-    @tag_context = TagContext.find(params[:id])
+    tag_context = TagContext.find(params[:id])
+    tag_context.update_attributes(params[:tag_context])
 
-    respond_to do |format|
-      if @tag_context.update_attributes(params[:tag_context])
-        format.html { redirect_to(@tag_context, :notice => 'TagContext was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @tag_context.errors, :status => :unprocessable_entity }
-      end
-    end
+    render json: tag_context.to_json, :content_type => 'application/json'
   end
 
-  # DELETE /tag_contexts/1
-  # DELETE /tag_contexts/1.xml
   def destroy
-    @tag_context = TagContext.find(params[:id])
-    @tag_context.destroy
+    tag_context = TagContext.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to(tag_contexts_url) }
-      format.xml  { head :ok }
+    if ((Person.tag_counts_on(tag_context.name).size > 0) || (ProgrammeItem.tag_counts_on(tag_context.name).size > 0))
+      render status: :bad_request, text: 'Unable to context as it is in use'
+    else
+      tag_context.destroy
+      render status: :ok, text: {}.to_json
     end
   end
 end
