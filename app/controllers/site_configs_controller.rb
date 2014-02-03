0b065974c739cee54346b9c2d2a62ea4d2796153
Henry Balen <balen@acm.org>
2014-02-03 22:35:15 +0000

    fix for intial case when there is no config
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1392 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/site_configs_controller.rb
+++ app/controllers/site_configs_controller.rb
@@ -3,7 +3,9 @@ class SiteConfigsController < PlannerController
     # TODO - when we have multi-tenant base this on the current convention...
     site_config = SiteConfig.find :first
     
-    site_config.start_date  = Date.today if !site_config.start_date
+    if site_config
+      site_config.start_date  = Date.today if !site_config.start_date
+    end
     
     render json: site_config.to_json, :content_type => 'application/json'
   end
@@ -11,7 +13,9 @@ class SiteConfigsController < PlannerController
   def show
     site_config = SiteConfig.find(params[:id])
 
-    site_config.start_date  = Date.today if !site_config.start_date
+    if site_config
+      site_config.start_date  = Date.today if !site_config.start_date
+    end
     
     render json: site_config.to_json, :content_type => 'application/json'
   end
