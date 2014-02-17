8a0cb84a017b950076a30f7b90ae738c102dd591
Henry Balen <balen@acm.org>
2014-02-17 23:18:58 +0000

    authorization rules fixed
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1500 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -2,34 +2,37 @@ authorization do
   
   role :Viewer do
     has_permission_on :addresses, :to => [:read]
+    has_permission_on :availabilities, :to => [:read]
+    has_permission_on :available_dates, :to => [:read]
     has_permission_on :edited_bios, :to => [:read]
     has_permission_on :email_addresses, :to => [:read]
+    has_permission_on :equipment_types, :to => :read
+    has_permission_on :equipment_needs, :to => :read
+    has_permission_on :excluded_items, :to => [:read]
+    has_permission_on :excluded_items_survey_maps, :to => [:read]
     has_permission_on :formats, :to => [:read]
     has_permission_on :invitation_categories, :to => [:read]
     has_permission_on :item_planner, :to => [:read]
     has_permission_on :pending_import_people, :to => [:read]
     has_permission_on :people, :to => [:read]
     has_permission_on :phone_numbers, :to => [:read]
+    has_permission_on :planner_reports, :to => [:read]
     has_permission_on :postal_addresses, :to => [:read]
     has_permission_on :programme_items, :to => [:read]
     has_permission_on :program_planner, :to => [:read]
+    has_permission_on :publisher, :to => :review
     has_permission_on :registration_details, :to => [:read]
     has_permission_on :roles, :to => [:read]
     has_permission_on :rooms, :to => [:read]
+    has_permission_on :room_setups, :to => :read
+    has_permission_on :survey_query, :to => :read
     has_permission_on :survey_respondents, :to => :read
     has_permission_on :survey_respondents_reviews, :to => [:read]
+    has_permission_on :survey_reports, :to => [:read]
     has_permission_on :tag_contexts, :to => [:read]
     has_permission_on :tags, :to => [:read]
     has_permission_on :venue, :to => [:read]
-    has_permission_on :survey_reports, :to => [:read]
-    has_permission_on :planner_reports, :to => [:read]
-    has_permission_on :availabilities, :to => [:read]
-    has_permission_on :excluded_items, :to => [:read]
-    has_permission_on :excluded_items_survey_maps, :to => [:read]
-    has_permission_on :available_dates, :to => [:read]
-    has_permission_on :equipment_types, :to => :read
-    has_permission_on :equipment_needs, :to => :read
-    has_permission_on :publisher, :to => :review
+    
     has_permission_on :pages_home_dash, :to => :read
     has_permission_on :pages_venues_dash, :to => :read
     has_permission_on :pages_participants_dash, :to => :read
@@ -38,8 +41,6 @@ authorization do
     has_permission_on :pages_surveys_dash, :to => :read
     has_permission_on :pages_reports_dash, :to => :read
     has_permission_on :pages_publications_dash, :to => :read
-    has_permission_on :survey_query, :to => :read
-    has_permission_on :room_setups, :to => :read
   end
 
   role :TagAdmin do
@@ -56,7 +57,7 @@ authorization do
   end
   
   role :Planner do
-    includes :Viewer
+    includes :Itemeditor
     has_permission_on :addresses, :to => :manage
     has_permission_on :edited_bios, :to => :manage
     has_permission_on :email_addresses, :to => :manage
@@ -67,7 +68,6 @@ authorization do
     has_permission_on :people, :to => :manage
     has_permission_on :phone_numbers, :to => :manage
     has_permission_on :postal_addresses, :to => :manage
-    has_permission_on :programme_items, :to => :manage
     has_permission_on :program_planner, :to => :manage
     has_permission_on :registration_details, :to => :manage
     has_permission_on :roles, :to => [:read]
@@ -75,7 +75,6 @@ authorization do
     has_permission_on :survey_respondents, :to => :manage
     has_permission_on :survey_respondents_reviews, :to => :manage
     has_permission_on :tag_contexts, :to => :manage
-    has_permission_on :tags, :to => :manage
     has_permission_on :venue, :to => :manage
     has_permission_on :survey_reports, :to => :manage
     has_permission_on :planner_reports, :to => :manage
@@ -83,14 +82,14 @@ authorization do
     has_permission_on :available_dates, :to => :manage
     has_permission_on :excluded_items_survey_maps, :to => [:manage]
     has_permission_on :programme_items, :to => :schedule
-    has_permission_on :equipment_types, :to => :manage
-    has_permission_on :equipment_needs, :to => :manage
     has_permission_on :publisher, :to => :schedule
     has_permission_on :emailreports, :to => :read
     has_permission_on :survey_query, :to => :manage
     has_permission_on :survey_reports, :to => :report
     has_permission_on :pages_communications_dash, :to => :read
-    
+    has_permission_on :communications_mailing, :to => :read
+    has_permission_on :communications_mail_history, :to => :read
+    has_permission_on :communications_mail_templates, :to => :read
   end
   
   role :SuperPlanner do
@@ -135,9 +134,9 @@ privileges do
     privilege :review, :includes => [:review]
     privilege :report, :includes => [:library_talks,:missing_bio,:moderators,:art_night,:music_night,:program_types,:free_text,:tags_by_context,:available_during,:panels_with_panelists,:panelists_with_panels,
         :admin_tags_by_context,:panels_date_form,:panelists_with_metadata,:interviewable,:panels_by_room,:panels_by_timeslot,:failed,:sent, 
-        :surveyNames, :surveyQueryNames, :show, :questions, :runReport, :delSurveyQuery]
+        :surveyNames, :surveyQueryNames, :show, :questions, :runReport, :delSurveyQuery, :table_tents, :people_by_tag, :badge_labels, :room_signs]
     privilege :read, :includes => [:report, :index, :show, :list, :listwithblank, :comphrensive,:acceptancestatuslist,:acceptancestatuslistwithblank,:ReportInviteStatus,
-        :doReportInviteStatus,:invitestatuslist,:invitestatuslistwithblank,:getConflicts,:getRoomControl, :updateSelect, :getList, :list, :assignments]
+        :doReportInviteStatus,:invitestatuslist,:invitestatuslistwithblank,:getConflicts,:getRoomControl, :updateSelect, :getList, :list, :assignments, :listWithBlank]
     privilege :manage, :includes => [:create, :read, :update, :delete, :communicate, :add, :remove, :updateParticipants,
                                       :SetInvitePendingToInvited, :doSetInvitePendingToInvited, :addItem, :removeItem,:program_book_report]
     privilege :create, :includes => [:new,:import,:doimport]
