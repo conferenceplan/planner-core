f6efc45f3c2e4075a5a690b38ffedc0799655133
Henry Balen <balen@acm.org>
2014-02-23 21:58:03 +0000

    test for cloudinary
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1529 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/services/config_service.rb
+++ app/services/config_service.rb
@@ -9,7 +9,7 @@ module ConfigService
   def self.hasCloudinaryConfig?
     cloud_config = CloudinaryConfig.find :first
 
-    cloud_config.api_key != nil && !cloud_config.api_key.empty?
+    cloud_config && cloud_config.api_key != nil && !cloud_config.api_key.empty?
   end
 
 end
