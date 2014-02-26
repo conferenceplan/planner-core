13e46c1bdbfdcec3d896e367291e6a8d9387099d
Henry Balen <balen@acm.org>
2014-02-26 01:03:19 +0000

    logging for delayed jobs
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1548 1f962665-22ff-41fc-9765-37673d54b4f3

--- config/initializers/delayed_jobs_config.rb
+++ config/initializers/delayed_jobs_config.rb
@@ -1,5 +1,10 @@
+require 'log_plugin'
+
 # config/initializers/delayed_job_config.rb
 Delayed::Worker.destroy_failed_jobs = false
-#Delayed::Worker.sleep_delay = 60
+Delayed::Worker.sleep_delay = 30 # Change do that delayed job only checks every 30 secs instead of 5
 #Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_run_time = 10.minutes
+
+# A 'plugin' that will help with directing the logging to a new file    
+Delayed::Worker.plugins << LogPlugin
---
+++ lib/log_plugin.rb
@@ -0,0 +1,19 @@
+#
+#
+#
+require 'delayed_job'
+
+class LogPlugin < Delayed::Plugin
+    callbacks do |lifecycle|
+        lifecycle.around(:invoke_job) do |job, *args, &block|
+            begin
+                PlannerRc1::Application.config.logger = ActiveRecord::Base.logger = Logger.new(File.join(Rails.root, 'log', 'jobs.log'))
+                # Forward the call to the next callback in the callback chain
+                block.call(job, *args)
+            rescue Exception => error
+                # Make sure we propagate the failure!
+                raise error
+            end
+        end
+    end
+end
