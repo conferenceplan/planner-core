37b502e31afc1e7b009e5949d30ae3cac173ac4d
Henry Balen <balen@acm.org>
2014-03-18 16:55:37 +0000

    fix for survey history
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1676 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/models/survey_history.rb
+++ app/models/survey_history.rb
@@ -1,5 +1,5 @@
 class SurveyHistory < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :filled_at, :survey_respondent_detail_id, :survey_id
   
   belongs_to :survey_respondent_detail
   belongs_to :survey
