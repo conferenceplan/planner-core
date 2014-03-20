aba3176dfd096d383ff6a6357aaf36a5ad97da68
Henry Balen <balen@acm.org>
2014-03-20 20:01:39 +0000

    mobile theme enhancements
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1690 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/models/mobile_theme.rb
+++ app/models/mobile_theme.rb
@@ -2,7 +2,7 @@ class MobileTheme < ActiveRecord::Base
   attr_accessible :action_bar_bg, :action_bar_text, :body_text, :body_text_secondary, :created_at, :date_text, :day_text, :favourite_off, :favourite_off_bg, 
                   :favourite_on_bg, :hot_ribbon_text, :id, :item_format, :item_name, :new_ribbon, :new_ribbon_text, :page_flipper_bg, :page_flipper_selection_bar, 
                   :page_flipper_separators, :page_flipper_text, :time_text, :updated_at, :updated_ribbon_text, :card_background, :card_shadow, :favourite_on, 
-                  :hot_ribbon, :main_background, :updated_ribbon, :default_bio_image
+                  :hot_ribbon, :main_background, :updated_ribbon, :default_bio_image, :menu_drawer_bg, :menu_drawer_selected, :menu_drawer_text
   attr_accessor :scale
 
   mount_uploader :default_bio_image, BioPictureUploader
