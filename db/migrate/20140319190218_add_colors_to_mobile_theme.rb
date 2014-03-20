fcc4655ca25b8767852b59f7f00cdbbd5be930d1
Henry Balen <balen@acm.org>
2014-03-20 19:29:22 +0000

    mobile theme enhancements
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1685 1f962665-22ff-41fc-9765-37673d54b4f3

---
+++ db/migrate/20140319190218_add_colors_to_mobile_theme.rb
@@ -0,0 +1,7 @@
+class AddColorsToMobileTheme < ActiveRecord::Migration
+  def change
+    add_column :mobile_themes, :menu_drawer_bg, :string, :default => "rgba(239,239,239,1)"
+    add_column :mobile_themes, :menu_drawer_selected, :string, :default => "rgba(0,173,329,1)"
+    add_column :mobile_themes, :menu_drawer_text, :string, :default => "rgba(35,31,32,1)"
+  end
+end
