b8c55ae16753517a3d09520d3c34a8dfa2152d55
Henry Balen <balen@acm.org>
2014-03-23 02:26:12 +0000

    better error handling
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1723 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/models/person.rb
+++ app/models/person.rb
@@ -84,6 +84,15 @@ class Person < ActiveRecord::Base
   has_one :peoplesource, :dependent => :delete
   has_one :datasource, :through => :peoplesource
 
+  before_destroy :check_if_assigned
+
+  # check that the person has not been assigned to program items, if they have then return an error and do not delete
+  def check_if_assigned
+    if programmeItemAssignments.size > 0
+      raise 'Can not delete a person that has been assigned to programme items'
+    end
+  end
+
   def key
     if survey_respondent
       return survey_respondent.key
