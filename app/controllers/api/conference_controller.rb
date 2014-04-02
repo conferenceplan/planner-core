dfff68afcf794d11436d228c65824e2b695007ea
Henry Balen <balen@acm.org>
2014-04-02 20:10:24 +0000

    fix for bio image in theme
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1808 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/api/conference_controller.rb
+++ app/controllers/api/conference_controller.rb
@@ -2,7 +2,8 @@ class Api::ConferenceController < ApplicationController
   def index
     @mobilePages = MobilePage.find :all, :order => 'position asc'
     @scale = params[:scale].to_f
-    @theme = MobileTheme.find :first # TODO - change, get the default
+    @theme = MobileTheme.where('isdefault = ?', true).first
+    @theme = MobileTheme.find :first if !@theme
     @formats = Format.find :all
     @default_bio_image = DefaultBioImage.find :first
     @cloudinaryURI = Cloudinary::Utils.cloudinary_url('A').sub(/\/A/,'')
@@ -12,7 +13,8 @@ class Api::ConferenceController < ApplicationController
   def show
     @mobilePages = MobilePage.find :all, :order => 'position asc'
     @scale = params[:scale].to_f
-    @theme = MobileTheme.find :first # TODO - change, get the default
+    @theme = MobileTheme.where('isdefault = ?', true).first
+    @theme = MobileTheme.find :first if !@theme
     @default_bio_image = DefaultBioImage.find :first
     @formats = Format.find :all
     @cloudinaryURI = Cloudinary::Utils.cloudinary_url('A').sub(/\/A/,'')
