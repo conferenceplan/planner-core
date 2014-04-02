64d0b46b8b08d31b6aafaf2a2d7b018efe92cd16
Henry Balen <balen@acm.org>
2014-04-02 14:09:24 +0000

    dash updates
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1785 1f962665-22ff-41fc-9765-37673d54b4f3

---
+++ app/services/audit_service.rb
@@ -0,0 +1,17 @@
+#
+#
+#
+module AuditService
+  
+  #
+  #
+  #
+  def self.getItemChangeHistory(count = 20)
+    
+    # Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ?", 'ProgrammeItem')
+    Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment')
+                                          .order("created_at desc").limit(count)
+                                          
+  end
+
+end
