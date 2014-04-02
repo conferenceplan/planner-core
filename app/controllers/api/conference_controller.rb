0a893e5c150bf2ca42bd91fd615d4aee7a954acf
Henry Balen <balen@acm.org>
2014-04-02 18:58:28 +0000

    fix for default bio images
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1797 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/api/conference_controller.rb
+++ app/controllers/api/conference_controller.rb
@@ -2,8 +2,9 @@ class Api::ConferenceController < ApplicationController
   def index
     @mobilePages = MobilePage.find :all, :order => 'position asc'
     @scale = params[:scale].to_f
-    @theme = MobileTheme.find :first
+    @theme = MobileTheme.find :first # TODO - change, get the default
     @formats = Format.find :all
+    @default_bio_image = DefaultBioImage.find :first
     @cloudinaryURI = Cloudinary::Utils.cloudinary_url('A').sub(/\/A/,'')
     @partition_val = @cloudinaryURI.sub(/http\:\/\/a[0-9]*\./,'')
   end
@@ -11,7 +12,8 @@ class Api::ConferenceController < ApplicationController
   def show
     @mobilePages = MobilePage.find :all, :order => 'position asc'
     @scale = params[:scale].to_f
-    @theme = MobileTheme.find :first
+    @theme = MobileTheme.find :first # TODO - change, get the default
+    @default_bio_image = DefaultBioImage.find :first
     @formats = Format.find :all
     @cloudinaryURI = Cloudinary::Utils.cloudinary_url('A').sub(/\/A/,'')
     @partition_val = @cloudinaryURI.sub(/http\:\/\/a[0-9]*\./,'')
