11f9f1fe8188d76cd3e3dde00c2ea99139ad02ac
Henry Balen <balen@acm.org>
2014-04-02 21:50:16 +0000

    IE check
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1809 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/cells/schedule/manage_cell.rb
+++ app/cells/schedule/manage_cell.rb
@@ -1,6 +1,12 @@
+require "browser"
+
 class Schedule::ManageCell < Cell::Rails
 
   def display
+    @browser = Browser.new(
+        accept_language: request.headers["Accept-Language"],
+        ua: request.headers["User-Agent"]
+      )
     render
   end
   
