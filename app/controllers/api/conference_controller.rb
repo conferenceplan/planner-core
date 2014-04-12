e3677fd7104b61e829a8da2d68bee4dd58e74a59
Henry Balen <balen@acm.org>
2014-04-12 03:25:32 +0000

    add conference logo management for mobile
    git-svn-id: http://svn.code.sf.net/p/conferenceplan/code/branches/planner-rc1@1847 1f962665-22ff-41fc-9765-37673d54b4f3

--- app/controllers/api/conference_controller.rb
+++ app/controllers/api/conference_controller.rb
@@ -4,6 +4,7 @@ class Api::ConferenceController < ApplicationController
     @scale = params[:scale].to_f
     @theme = MobileTheme.where('isdefault = ?', true).first
     @theme = MobileTheme.find :first if !@theme
+    @logo = ConferenceLogo.find :first
     @formats = Format.find :all
     @default_bio_image = DefaultBioImage.find :first
     @cloudinaryURI = Cloudinary::Utils.cloudinary_url('A').sub(/\/A/,'')
@@ -15,6 +16,7 @@ class Api::ConferenceController < ApplicationController
     @scale = params[:scale].to_f
     @theme = MobileTheme.where('isdefault = ?', true).first
     @theme = MobileTheme.find :first if !@theme
+    @logo = ConferenceLogo.find :first
     @default_bio_image = DefaultBioImage.find :first
     @formats = Format.find :all
     @cloudinaryURI = Cloudinary::Utils.cloudinary_url('A').sub(/\/A/,'')
