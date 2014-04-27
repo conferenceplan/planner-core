787fc12e73f87948e1bc9a5928598a4692dc5733
Henry Balen <henry@grenadinetech.com>
2014-04-27 09:37:52 -0400

    Add support for decorators

--- lib/planner_core/engine.rb
+++ lib/planner_core/engine.rb
@@ -71,5 +71,11 @@ module PlannerCore
       end
     end
 
+    #
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
