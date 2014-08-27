9896ebeb870792cc8d6ae0b1bc3f77d5be8dabf1
Henry Balen <henry@grenadinetech.com>
2014-08-27 16:57:56 -0400

    bug fixes

--- app/models/survey_history.rb
+++ app/models/survey_history.rb
@@ -1,5 +1,5 @@
 class SurveyHistory < ActiveRecord::Base
-  attr_accessible :filled_at, :survey_respondent_detail_id, :survey_id
+  attr_accessible :filled_at, :survey_respondent_detail_id, :survey_id, :survey_respondent_detail, :survey
   
   belongs_to :survey_respondent_detail
   belongs_to :survey
