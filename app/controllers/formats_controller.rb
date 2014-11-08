13b1bc0206fdff448587c2e9c1d5570f017e9fa1
Henry Balen <henry@grenadinetech.com>
2014-11-07 20:07:06 -0500

    generic resource controller

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -1,50 +1,5 @@
-class FormatsController < PlannerController
+class FormatsController < ResourceController
   
-  #
-  #
-  #
-  def index
-    @formats = Format.find :all, :order => 'position asc'
-  end
-  
-  #
-  #
-  #
-  def show
-    @format = Format.find(params[:id])
-  end
-  
-  #
-  #
-  #
-  def create
-    @format = Format.new(params[:format])
-    @format.save!
-  end
-  
-  #
-  #
-  #
-  def update
-    @format = Format.find(params[:id])
-    @format.update_attributes(params[:format])
-  end
-
-  #
-  #
-  #  
-  def destroy
-    format = Format.find(params[:id])
-
-    begin
-      format.destroy
-      render status: :ok, text: {}.to_json
-    rescue => ex
-      render status: :bad_request, text: ex.message
-    end
-  end
-
-########  
   def list  
     # Get all the formats in the database
     @formats = Format.find :all, :order => 'position asc'
@@ -56,4 +11,10 @@ class FormatsController < PlannerController
     @formats = Format.find :all , :order => 'position asc'
     render :layout => 'content'
   end
+
+  # make sure that the collection is ordered by the position
+  def collection
+    super.order(:position)
+  end
+
 end
