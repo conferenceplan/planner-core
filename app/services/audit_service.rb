b0520b1dfd0cf8d1328140f218fd6ce091e61f09
Henry Balen <henry@grenadinetech.com>
2014-11-20 10:08:49 -0500

    for muti-tenant

--- app/services/audit_service.rb
+++ app/services/audit_service.rb
@@ -9,7 +9,7 @@ module AuditService
   def self.getItemChangeHistory(count = 20)
     
     # Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ?", 'ProgrammeItem')
-    Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment')
+    Audited::Adapters::ActiveRecord::Audit.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment')
                                           .order("created_at desc").limit(count)
                                           
   end
