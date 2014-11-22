17a45763e113bc0699f176a2e6acc76a1c00b683
Henry Balen <henry@grenadinetech.com>
2014-11-22 09:05:50 -0500

    fix for query order in audit

--- app/services/audit_service.rb
+++ app/services/audit_service.rb
@@ -9,8 +9,8 @@ module AuditService
   def self.getItemChangeHistory(count = 20)
     
     # Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ?", 'ProgrammeItem')
-    Audited::Adapters::ActiveRecord::Audit.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment')
-                                          .order("created_at desc").limit(count)
+    Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment')
+                                          .order("created_at desc, version desc").limit(count)
                                           
   end
 
