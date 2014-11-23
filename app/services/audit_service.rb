14a6c16d8b291a81177cdac862abccbaba61605a
Henry Balen <henry@grenadinetech.com>
2014-11-23 13:31:05 -0500

    add constraint hook for audit service

--- app/services/audit_service.rb
+++ app/services/audit_service.rb
@@ -8,10 +8,14 @@ module AuditService
   #
   def self.getItemChangeHistory(count = 20)
     
-    # Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ?", 'ProgrammeItem')
-    Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment')
-                                          .order("created_at desc, version desc").limit(count)
+    Audited::Adapters::ActiveRecord::Audit.unscoped.where("auditable_type = ? OR auditable_type = ?", 'ProgrammeItem', 'ProgrammeItemAssignment').
+                                          where(self.constraints()).
+                                          order("created_at desc, version desc").limit(count)
                                           
+  end
+
+  def self.constraints(*args)
+    ''
   end
 
 end
