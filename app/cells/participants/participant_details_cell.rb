c09e0e14fc8e5d18cc6469cf0b3d7852ef0bbb89
Henry Balen <henry@grenadinetech.com>
2015-01-22 15:41:30 -0500

    fixes for extra content

--- app/cells/participants/participant_details_cell.rb
+++ app/cells/participants/participant_details_cell.rb
@@ -1,15 +1,19 @@
 class Participants::ParticipantDetailsCell < Cell::Rails
 
   def display
+    @extra_tab_display = [] if ! @extra_tab_display
     render
   end
   
   def javascript
+    @extra_javascript = [] if ! @extra_javascript
     @start_date = SiteConfig.first
     render
   end
   
   def templates
+    @extra_template = [] if !@extra_template
+    @extra_tab_template = [] if !@extra_tab_template
     render
   end
 
