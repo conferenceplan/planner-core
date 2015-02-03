8976d79956426df4c6997d86eea0d06c1ee9dfa3
Henry Balen <henry@grenadinetech.com>
2015-02-03 13:44:22 -0500

    rework of menu structure

--- lib/planner/controller_additions.rb
+++ lib/planner/controller_additions.rb
@@ -33,24 +33,22 @@ module Planner
       ''
     end
     
-    def top_menu_additions
-      return {}
-    end
-
     def top_menu
       #  return the hash for the top menu
-      return {
-        ""                 => ({ :target => '/',           :icon => "glyphicon-home",      :target_base => '/'} if allowed? :venues),
-        "venues"           => ({ :target => '/pages/venues_dash/manage',           :icon => "glyphicon-th-list",   :target_base => '/pages/venues_dash'} if allowed? :venues),
-        "participants"     => ({ :target => '/pages/participants_dash/manage',     :icon => "glyphicon-user",      :target_base => '/pages/participants_dash'} if allowed? :participants),
-        "program-items"    => ({ :target => '/pages/items_dash/manage',            :icon => "glyphicon-tasks",     :target_base => '/pages/items_dash'} if allowed? :items),
-        "schedule"         => ({ :target => '/pages/schedule_dash/manage',         :icon => "glyphicon-calendar",  :target_base => '/pages/schedule_dash'} if allowed? :schedule),
-        "surveys"          => ({ :target => '/pages/surveys_dash/report',          :icon => "glyphicon-pencil",    :target_base => '/pages/surveys_dash'} if allowed? :surveys),
-        "communications"   => ({ :target => '/pages//communications_dash/manage',  :icon => "glyphicon-envelope",  :target_base => '/pages/communications_dash'} if allowed? :communications),
-        "reports"          => ({ :target => '/pages/reports_dash/manage',          :icon => "glyphicon-stats",     :target_base => '/pages/reports_dash'} if allowed? :reports),
-        "publications"     => ({ :target => '/pages/publications_dash/print',      :icon => "glyphicon-print",     :target_base => '/pages/publications_dash'} if allowed? :publications),
-        "admin"            => ({ :target => '/pages/admin_dash/configs',           :icon => "glyphicon-wrench",    :target_base => '/pages/admin_dash'} if allowed? :admin)
-      }.keep_if{|k,v| v}.merge!(top_menu_additions)
+      menu = []
+      
+      menu << { :title => '',               :target => '/',                                   :icon => "glyphicon-home",      :target_base => '/'}
+      menu << { :title => 'venues',         :target => '/pages/venues_dash/manage',           :icon => "glyphicon-th-list",   :target_base => '/pages/venues_dash'} if allowed? :venues
+      menu << { :title => 'participants',   :target => '/pages/participants_dash/manage',     :icon => "glyphicon-user",      :target_base => '/pages/participants_dash'} if allowed? :participants
+      menu << { :title => 'program-items',  :target => '/pages/items_dash/manage',            :icon => "glyphicon-tasks",     :target_base => '/pages/items_dash'} if allowed? :items
+      menu << { :title => 'schedule',       :target => '/pages/schedule_dash/manage',         :icon => "glyphicon-calendar",  :target_base => '/pages/schedule_dash'} if allowed? :schedule
+      menu << { :title => 'surveys',        :target => '/pages/surveys_dash/report',          :icon => "glyphicon-pencil",    :target_base => '/pages/surveys_dash'} if allowed? :surveys
+      menu << { :title => 'communications', :target => '/pages//communications_dash/manage',  :icon => "glyphicon-envelope",  :target_base => '/pages/communications_dash'} if allowed? :communications
+      menu << { :title => 'reports',        :target => '/pages/reports_dash/manage',          :icon => "glyphicon-stats",     :target_base => '/pages/reports_dash'} if allowed? :reports
+      menu << { :title => 'publications',   :target => '/pages/publications_dash/print',      :icon => "glyphicon-print",     :target_base => '/pages/publications_dash'} if allowed? :publications
+      menu << { :title => 'admin',          :target => '/pages/admin_dash/configs',           :icon => "glyphicon-wrench",    :target_base => '/pages/admin_dash'} if allowed? :admin
+
+      menu
     end
       
   end
