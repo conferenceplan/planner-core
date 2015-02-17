29bc2cde76cda7694b86737bc55be96ba707be21
Henry Balen <henry@grenadinetech.com>
2015-02-17 13:50:00 -0500

    show settings only when logged in

--- lib/planner/controller_additions.rb
+++ lib/planner/controller_additions.rb
@@ -82,9 +82,11 @@ module Planner
     def settings_menu
       menu = []
       
-      menu << { :title => 'my-profile', :target =>  baseUri + '/users/edit' } if current_user
-      menu << { :title => 'event-setup', :target => baseUri + '/pages/admin_dash/configs' } if allowed? :event_setup
-      menu << { :title => 'system-settings', :target => '/pages/admin_dash/system_configs'} if allowed? :system_settings
+      if current_user
+        menu << { :title => 'my-profile', :target =>  baseUri + '/users/edit' } if current_user
+        menu << { :title => 'event-setup', :target => baseUri + '/pages/admin_dash/configs' } if allowed? :event_setup
+        menu << { :title => 'system-settings', :target => '/pages/admin_dash/system_configs'} if allowed? :system_settings
+      end
       
       menu
     end
