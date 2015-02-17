3bea1a8eca1cfa1cd9aaeb7a9706836276e4dc12
Henry Balen <henry@grenadinetech.com>
2015-02-17 11:18:50 -0500

    menu refactoring

--- app/cells/admin/configs_cell.rb
+++ app/cells/admin/configs_cell.rb
@@ -1,15 +1,18 @@
 class Admin::ConfigsCell < Cell::Rails
   
   def display
+    @extra_system_config_menu = [] if !@extra_system_config_menu
     render
   end
 
   def javascript
+    @extra_javascript = [] if !@extra_javascript
     @start_date = SiteConfig.first
     render
   end
 
   def templates
+    @extra_template = [] if !@extra_template
     render
   end
 
