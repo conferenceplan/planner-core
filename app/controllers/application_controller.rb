cc859cc362fd59ac3a67533b4ca5d30009848ee0
Henry Balen <henry@grenadinetech.com>
2015-03-04 11:59:13 -0500

    support for apis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,55 +2,10 @@
 #
 #
 class ApplicationController < ActionController::Base
+  include Planner::ControllerExtensions
+  
   before_filter :first_filter, :set_locale, :load_configs, :set_mailer_host, :last_filter
   around_filter :application_time_zone # make sure that we use the timezone as specified in the database
-  
-  def get_base_image_url
-    eval(ENV[:base_image_url.to_s])
-  end
-  
-  def first_filter
-    # do nowt
-    logger.debug 'ACTION: ' + params[:action]
-    logger.debug 'PARMS: ' + params.to_s
-  end
-
-  def last_filter
-    # do nowt
-  end
-
-  def set_mailer_host
-    ActionMailer::Base.default_url_options[:host] = request.host_with_port
-  end
-   
-  def set_locale
-    I18n.locale = (params[:locale] && params[:locale].size > 0)? params[:locale] : I18n.default_locale
-  end
-  
-  def application_time_zone(&block)
-    cfg = SiteConfig.find :first # for now we only have one convention... change when we have many
-    zone = cfg ? cfg.time_zone : Time.zone
-    Time.use_zone(zone, &block)
-  end
-  
-  def load_configs
-    cfg = CloudinaryConfig.find :first # for now we only have one convention... change when we have many
-    Cloudinary.config do |config|
-      config.cloud_name           = cfg ? cfg.cloud_name : ''
-      config.api_key              = cfg ? cfg.api_key : ''
-      config.api_secret           = cfg ? cfg.api_secret : ''
-      config.enhance_image_tag    = cfg ? cfg.enhance_image_tag : false
-      config.static_image_support = cfg ? cfg.static_image_support : false
-     config.cdn_subdomain = true
-    end
-    
-    mail_cfg = MailConfig.find :first
-    if mail_cfg
-      Devise.setup do |config|
-        config.mailer_sender = mail_cfg.from
-      end
-    end
-  end
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -59,11 +14,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_respondent
   
   def current_user
+    user = nil
     if support_user_signed_in?
-      current_support_user
+      user = current_support_user
     else
-      @current_user ||= warden.authenticate(scope: :user)
+      user = @current_user ||= warden.authenticate(scope: :user)
     end
+    user
   end
   
   private
