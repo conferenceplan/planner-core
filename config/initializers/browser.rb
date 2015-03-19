187fe6d657aba640888f0ac39474b0d75eb11594
Henry Balen <henry@grenadinetech.com>
2015-03-19 15:35:49 -0400

    test for android and ios

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,4 +1,4 @@
 Rails.configuration.middleware.use Browser::Middleware do
   # NewRelicPinger
-  redirect_to "/upgrade.html" unless browser.modern? || browser.user_agent.include?('NewRelicPinger')
+  redirect_to "/upgrade.html" unless browser.modern? || browser.user_agent.include?('NewRelicPinger') || browser.android? || browser.ios?
 end
