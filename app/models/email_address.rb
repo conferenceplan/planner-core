e4fa94805ec0c59f91a76a0e94005e404900dc0a
Henry Balen <henry@grenadinetech.com>
2015-03-20 15:26:43 -0400

    fix for user agent link from address to people

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -3,4 +3,7 @@ class EmailAddress < ActiveRecord::Base
 #  has_many :addresses, :as => :addressable
 #  has_many :people, :through => :addresses
   audited :allow_mass_assignment => true
+
+  has_many :addresses, as: :addressable
+  has_many :people, through: :addresses
 end
