545f6fb1d025b91f70b9cb5eab99e06a61b038f5
Henry Balen <henry@grenadinetech.com>
2015-03-20 15:30:49 -0400

    add CFNetwork for UA

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,4 +1,4 @@
 Rails.configuration.middleware.use Browser::Middleware do
   # NewRelicPinger
-  redirect_to "/upgrade.html" unless browser.modern? || browser.user_agent.include?('NewRelicPinger') || browser.mobile? || (browser.ua =~ /iphone/)
+  redirect_to "/upgrade.html" unless browser.modern? || browser.user_agent.include?('NewRelicPinger') || browser.mobile? || (browser.ua =~ /CFNetwork/)
 end
