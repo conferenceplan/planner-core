2e93df6f149ba23f8c9bce7a9e0abb2bc1d55b63
Henry Balen <henry@grenadinetech.com>
2015-03-31 17:10:37 -0400

    remove browser test

--- config/initializers/browser.rb
+++
@@ -1,4 +0,0 @@
-Rails.configuration.middleware.use Browser::Middleware do
-  # NewRelicPinger
-  redirect_to "/upgrade.html" unless browser.modern? || browser.user_agent.include?('NewRelicPinger') || browser.mobile? || (browser.ua =~ /CFNetwork/)
-end
