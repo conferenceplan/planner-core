c4a58f2c36c4da6e53948214a64ddc721be3b73c
Henry Balen <henry@grenadinetech.com>
2015-06-28 17:27:54 -0400

    update the boostrap-table to latest JS Add filter control option to bootstrap tables

--- app/assets/javascripts/application-core.js
+++ app/assets/javascripts/application-core.js
@@ -27,7 +27,8 @@
 //= require bootstrap
 
 //= require bootstrap-tabdrop
-//= require bootstrap-table/bootstrap-table.min
+//= require bootstrap-table/bootstrap-table
+//= require bootstrap-table/extensions/filter-control/bootstrap-table-filter-control
 
 //= require backbone/underscore-min
 //= require backbone/backbone-min
