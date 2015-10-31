65ebbfe4b7c22faef1e20c292a2e4b6ff93f8096
balen <henry@grenadinetech.com>
2015-10-31 09:44:50 -0400

    support serialization of assignments

--- app/models/published_programme_item_assignment.rb
+++ app/models/published_programme_item_assignment.rb
@@ -10,4 +10,21 @@ class PublishedProgrammeItemAssignment < ActiveRecord::Base
   belongs_to  :published_programme_item, :foreign_key => "published_programme_item_id"
 
   has_enumerated :role, :class_name => 'PersonItemRole'
+
+  def time_stamp
+    updated_at.utc
+  end
+  
+  def role_as_string
+    PersonItemRole.find(role_id).name
+  end
+
+  def serializable_hash(options = {})
+    super only: [], methods: [:time_stamp, :role_as_string], include: {
+          published_programme_item: {
+              only: [:id]
+          }
+        }
+  end
+
 end
--- lib/planner_core/version.rb
+++ lib/planner_core/version.rb
@@ -1,3 +1,3 @@
 module PlannerCore
-  VERSION = "0.8.20"
+  VERSION = "0.8.21"
 end
