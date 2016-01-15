5bb0c024a696fa8e351991557b2cc13736de6627
Rodrigo Machado <rodmac98@gmail.com>
2016-01-15 02:28:08 +0000

    Rooms sort_order creation repositioning

--- db/migrate/20141104181518_add_sort_order_venue.rb
+++ db/migrate/20141104181518_add_sort_order_venue.rb
@@ -1,7 +1,6 @@
 class AddSortOrderVenue < ActiveRecord::Migration
   def change
     add_column :venues, :sort_order, :integer, { :default => 0 }
-    add_column :rooms, :sort_order, :integer, { :default => 0 }
     add_column :published_venues, :sort_order, :integer, { :default => 0 }
     add_column :published_rooms, :sort_order, :integer, { :default => 0 }
   end
