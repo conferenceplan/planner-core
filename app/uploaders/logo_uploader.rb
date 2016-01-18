52aad1e0837b698b253dff24f192e0c7dc676e92
balen <henry@grenadinetech.com>
2016-01-18 11:09:42 -0500

    more logo uploader to core change cards crop to fit

---
+++ app/uploaders/logo_uploader.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+
+class LogoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave   # Use cloudinary as the image store
+  include UploaderHelper
+
+  #
+  # Use a combination of the person's name for the id of the image
+  #
+  def public_id
+    publicid = root_path + '/'
+    publicid += 'conf_logo'
+    publicid.gsub(/\s+/, "")
+  end
+  
+  #
+  #
+  #
+  version :standard do
+    process :standardImage
+  end
+  
+  version :full do
+    process :fullImage
+  end
+  
+  #
+  #
+  #
+  def fullImage
+    return :fetch_format => :jpg
+  end
+  
+  #
+  #
+  #
+  def standardImage
+    width = ((model.scale && model.scale > 0) ? 240 * model.scale : 240).to_i
+    height = ((model.scale && model.scale > 0) ? 100 * model.scale : 100).to_i
+    return :height => height, :width => width, :crop => :fit, :fetch_format => :jpg
+  end
+  
+end
