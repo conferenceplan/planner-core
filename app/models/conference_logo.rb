ee4f81ede33285c020f1116e561750080760a860
balen <henry@grenadinetech.com>
2016-02-01 22:06:59 -0500

    put conference logo back in

--- app/models/conference_logo.rb
+++ app/models/conference_logo.rb
@@ -1,6 +1,6 @@
-# class ConferenceLogo < ActiveRecord::Base
-#   attr_accessible :lock_version, :image
-#   attr_accessor :scale
+class ConferenceLogo < ActiveRecord::Base
+  attr_accessible :lock_version, :image
+  attr_accessor :scale
 
-#   mount_uploader :image, LogoUploader # Use Carrierwave to manage image uploads and retrieval (external storage with name in the db)
-# end
+  mount_uploader :image, LogoUploader # Use Carrierwave to manage image uploads and retrieval (external storage with name in the db)
+end
