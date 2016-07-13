59ab6399390862e98cf163b2dc7b73890008396c
balen <henry@grenadinetech.com>
2016-07-13 16:30:05 -0400

    exclusions do not need to be audited

--- app/models/excluded_items_survey_map.rb
+++ app/models/excluded_items_survey_map.rb
@@ -4,6 +4,4 @@ class ExcludedItemsSurveyMap < ActiveRecord::Base
   belongs_to :programme_item
   belongs_to :survey_answer
   
-  audited :associated_with => :programme_item, :allow_mass_assignment => true
-
 end
--- app/models/exclusion.rb
+++ app/models/exclusion.rb
@@ -2,6 +2,4 @@ class Exclusion < ActiveRecord::Base
   attr_accessible :lock_version, :source, :excludable_id, :excludable_type, :person_id
   belongs_to  :person
   belongs_to  :excludable, :polymorphic => true
-
-  audited :associated_with => :person
 end
