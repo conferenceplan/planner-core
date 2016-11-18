c0a3eb09bb415c8f1a39db22f0330bdd61d0f9fe
Robert <robert@grenadine.co>
2016-11-18 12:32:35 -0500

    fix for edited bio template refresh on create

--- app/controllers/edited_bios_controller.rb
+++ app/controllers/edited_bios_controller.rb
@@ -1,31 +1,34 @@
 class EditedBiosController < PlannerController
   
   def update
-    editedBio = EditedBio.find(params[:id])
+    @editedBio = EditedBio.find(params[:id])
     
-    editedBio.update_attributes(params[:edited_bio])
-
-    render json: editedBio.to_json, :content_type => 'application/json'
+    @editedBio.update_attributes(params[:edited_bio])
+    @surveyBio = @editedBio.person.GetSurveyBio
+  rescue => ex
+    render status: :bad_request, text: ex.message
   end
   
   def create
     if (params[:person_id])
-      person = Person.find(params[:person_id]) 
-      editedBio = person.create_edited_bio(params[:edited_bio]);
+      @person = Person.find(params[:person_id]) 
+      @editedBio = @person.create_edited_bio(params[:edited_bio])
+      @surveyBio = @person.GetSurveyBio
     else
-      editedBio = EditedBios.new(params[:edited_bio])
+      @editedBio = EditedBio.new(params[:edited_bio])
+      @surveyBio = @editedBio.person.GetSurveyBio
     end
     
-    editedBio.save!
-
-    render json: editedBio.to_json, :content_type => 'application/json'
+    @editedBio.save!
+  rescue => ex
+    render status: :bad_request, text: ex.message
   end
   
   def show
     if (params[:person_id])
-      person = Person.find(params[:person_id])
+      @person = Person.find(params[:person_id])
 
-      @editedBio = person.edited_bio
+      @editedBio = @person.edited_bio
       @surveyBio = @person.GetSurveyBio
     else
       @editedBio = EditedBio.find(params[:id])
