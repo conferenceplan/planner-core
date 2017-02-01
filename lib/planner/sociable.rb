8f1027c165f1f163c53f60ec500c8432be079e35
Robert <robert@grenadine.co>
2017-02-01 15:07:27 -0500

    dynamically define url methods for social media handles

--- lib/planner/sociable.rb
+++ lib/planner/sociable.rb
@@ -11,8 +11,10 @@ module Planner
         # :facebook, :twitter/:twitterinfo, :linkedin, :youtube, :twitch, 
         # :instagram, :flickr, :reddit, :othersocialmedia, :website, :url
 
+        ## Set accessible attributes
         attr_accessible *args
 
+        ## Handle twitter attr variation and define its method
         if args.include?(:twitter) || args.include?(:twitterinfo)
           send(:define_method, :twitter) do
             if self.has_attribute?(:twitter)
@@ -25,6 +27,7 @@ module Planner
           end
         end
         
+        ## Define ID methods for social media handles
         if args.include?(:facebook)
           send(:define_method, :facebookid) do
             /[^\/|^@]+$/.match(facebook.present? ? facebook.gsub(/\/+$/,'') : '').to_s
@@ -73,19 +76,7 @@ module Planner
           end
         end
 
-        if args.include?(:website) || args.include?(:url)
-          send(:define_method, :website_url) do
-            if self.has_attribute?(:website)
-              fix_url(self.read_attribute(:website))
-            elsif self.has_attribute?(:url)
-              fix_url(self.read_attribute(:url))
-            else
-              nil
-            end
-          end
-        end
-
-
+        ## Define fix_url method
         send(:define_method, :fix_url) do |url|
           if url.present?
             res = url.strip # remove trailing and preceding whitespace
@@ -101,6 +92,7 @@ module Planner
           end
         end
 
+        ## Define social media checker
         send(:define_method, :has_social_media?) do
           (self.respond_to?(:facebook) && facebook.present?) || 
           (self.respond_to?(:twitter) && twitter.present?) || 
@@ -111,6 +103,65 @@ module Planner
           (self.respond_to?(:flickr) && flickr.present?) || 
           (self.respond_to?(:reddit) && reddit.present?) || 
           (self.respond_to?(:othersocialmedia) && othersocialmedia.present?)
+        end
+
+        ## Base URLs for each social media provider
+        social_base_urls = {
+          :facebook => "http://www.facebook.com/",
+          :twitter => "http://www.twitter.com/",
+          :linkedin => "http://www.linkedin.com/in/",
+          :youtube => "http://www.youtube.com/user/", 
+          :twitch => "http://www.twitch.tv/", 
+          :instagram => "http://www.instagram.com/", 
+          :flickr => "http://www.flickr.com/photos/", 
+          :reddit => "http://www.reddit.com/user/"
+        }
+
+        ## For each social media provider set for the model, define url generator methods
+        args.each do |arg|
+          if arg == :url || arg == :website
+            send(:define_method, :website_url) do
+              if self.has_attribute?(:website)
+                fix_url(self.read_attribute(:website))
+              elsif self.has_attribute?(:url)
+                fix_url(self.read_attribute(:url))
+              else
+                nil
+              end
+            end
+          else
+            if arg == :twitter || arg == :twitterinfo
+              arg_name = 'twitter'
+            else
+              arg_name = arg.to_s
+            end
+            method_name = arg_name + '_url'
+
+            ## Define url method for provider
+            send(:define_method, method_name) do
+              url = ""
+              social_arg = arg_name.to_sym
+              if self.respond_to?(social_arg)
+                social = self.send(social_arg)
+                if social.present?
+                  if social[/\Ahttp:\/\//] || social[/\Ahttps:\/\//]
+                    ## If social attribute is already a url, use it directly
+                    url = social
+                  else
+                    ## If social attribute is not a url, build the url using one of the base urls defined in "social_base_urls" variable
+                    base = social_base_urls[social_arg]
+                    if base.present?
+                      social_id = self.send(social_arg.to_s + "id")
+                      url = base + social_id
+                    end
+                  end
+                end
+              end
+
+              url.gsub(/\/+$/,'')
+            end
+
+          end
         end
         
       end
