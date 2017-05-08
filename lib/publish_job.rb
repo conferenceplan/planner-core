117d71d3d0032886cd810d33b768ab0379daa199
Unknown <robert@grenadine.co>
2017-05-08 18:56:14 -0400

    Trying to fix the publish mechanism. Defer loading item models for links

--- lib/publish_job.rb
+++ lib/publish_job.rb
@@ -9,8 +9,20 @@ class PublishJob
   end
   
   def before(job)
+    ActiveSupport.on_load(:programme_item) do
+      Planner::Linkable.configure do |config|
+        config.addLinkable ProgrammeItem
+        ProgrammeItem.linked
+      end
+    end
+    ActiveSupport.on_load(:published_programme_item) do
+      Planner::Linkable.configure do |config|
+        config.addLinkable PublishedProgrammeItem
+        PublishedProgrammeItem.linked
+      end
+    end
     Planner::Linkable.configure do |config|
-      config.addLinkable [Person, ProgrammeItem, PublishedProgrammeItem, Venue, Room]
+      config.addLinkable [Person, Venue, Room]
     end
     ActiveRecord::Base.send(:include, Planner::Linkable)
     Planner::Linkable.setup
