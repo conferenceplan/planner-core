dfd9cf8adb600b3c6257909f350358bc303d9416
balen <henry@grenadinetech.com>
2017-05-09 13:24:45 -0400

    fix initialization for linkable

--- config/initializers/links.rb
+++ config/initializers/links.rb
@@ -3,18 +3,6 @@
 #
 require 'planner/linkable'
 
-ActiveSupport.on_load(:programme_item) do
-  Planner::Linkable.configure do |config|
-    config.addLinkable ProgrammeItem
-    ProgrammeItem.linked
-  end
-end
-ActiveSupport.on_load(:published_programme_item) do
-  Planner::Linkable.configure do |config|
-    config.addLinkable PublishedProgrammeItem
-    PublishedProgrammeItem.linked
-  end
-end
 Planner::Linkable.configure do |config|
-  config.addLinkable [Person, Venue, Room]
+  config.addLinkable ["Person", "ProgrammeItem", "PublishedProgrammeItem", "Venue", "Room"]
 end
--- lib/planner_core/version.rb
+++ lib/planner_core/version.rb
@@ -1,3 +1,3 @@
 module PlannerCore
-  VERSION = "1.3.64"
+  VERSION = "1.3.65"
 end
