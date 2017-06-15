d697ad79ad4a3a80566fa4dc4b7d4c0be5c7a8bc
balen <henry@grenadinetech.com>
2017-06-15 14:20:55 -0400

    add mail notification flag

--- app/models/mail_config.rb
+++ app/models/mail_config.rb
@@ -1,3 +1,3 @@
 class MailConfig < ActiveRecord::Base
-  attr_accessible :conference_name, :cc, :from, :domain, :info, :test_email, :reply_to
+  attr_accessible :conference_name, :cc, :from, :domain, :info, :test_email, :reply_to, :receive_notifications
 end
--- lib/planner_core/version.rb
+++ lib/planner_core/version.rb
@@ -1,3 +1,3 @@
 module PlannerCore
-  VERSION = "1.3.89"
+  VERSION = "1.3.90"
 end
