92bd17cb527ab815fc1e66e0ab5f5975bb1bbf70
Robert Smith <robert@grenadine.co>
2017-08-25 12:34:52 -0400

    removed deletion validator from mail template

--- app/models/mail_template.rb
+++ app/models/mail_template.rb
@@ -7,24 +7,12 @@ class MailTemplate < ActiveRecord::Base
   belongs_to :survey
   has_many :mailings
 
-  before_destroy :check_for_mailings
-
   def as_json(options={})
     res = super()
 
     res['mail_use'] = mail_use.name  
         
     return res
-  end
-
-private
-
-  # we  want to check that this has not been used by a mailing
-  def check_for_mailings
-    if mailings.any?
-      # errors.add :base, "Sorry, you can't delete a template that's being used in a mailing."
-      raise "Sorry, you can't delete a template that's being used in a mailing."
-    end
   end
 
 end
