220200fb076ba606d8a8d305ef0f8f1218c7f5e0
Unknown <robert@grenadine.co>
2017-11-13 22:54:40 -0500

    added class stubs for future feature additions

---
+++ app/models/babel/message.rb
@@ -0,0 +1,6 @@
+module Babel
+  # This class stub is for future feature implementation
+  class Message #< ActiveRecord::Base
+
+  end
+end
---
+++ app/models/babel/notification.rb
@@ -0,0 +1,6 @@
+module Babel
+  # This class stub is for future feature implementation
+  class Notification #< ActiveRecord::Base
+
+  end
+end
