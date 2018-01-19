3bb45e86998c329f4f634d83937060dc7f1c67a8
Robert Smith <robert@grenadine.co>
2018-01-19 04:26:19 -0500

    Added relationships for tags and taggings to tag context

--- app/models/tag_context.rb
+++ app/models/tag_context.rb
@@ -1,3 +1,8 @@
 class TagContext < ActiveRecord::Base
   attr_accessible :lock_version, :name, :publish
+  has_many :taggings,
+           foreign_key: 'context',
+           primary_key: :name,
+           class_name: 'ActsAsTaggableOn::Tagging'
+  has_many :tags, through: :taggings
 end
